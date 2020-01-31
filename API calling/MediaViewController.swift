3abeaf8b24b4c40ffd5ee7d1bf1610b47e6e5565
Riley Bowling <rbowling@bsd220.org>
2020-01-31 11:22:10 -0600

    set up initial storyboard

--- API calling/MediaViewController.swift
+++ API calling/MediaViewController.swift
@@ -1,14 +1,14 @@
 //
 //  ViewController.swift
 //  API calling
-//
+//  select what type of media -> select which one -> show media
 //  Created by Riley Bowling on 1/31/20.
 //  Copyright © 2020 Riley Bowling. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class MediaViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
